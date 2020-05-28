3026984963f32313ac0436c52f4ab98e0ec3f9f1
Ralph Slooten <axllent@gmail.com>
2020-05-28 16:00:40 +1200

    Fix deprecated implode() order

--- src/Forms/NoticeField.php
+++ src/Forms/NoticeField.php
@@ -1,13 +1,25 @@
 <?php
-
 namespace Axllent\FormFields\Forms;
 
 use SilverStripe\Forms\LiteralField;
 
 class NoticeField extends LiteralField
 {
+    /**
+     * @var mixed
+     */
     protected $content;
 
+    /**
+     * Contrustor
+     *
+     * @param string $name     Field name
+     * @param string $content  HTML Content
+     * @param string $class    Class string
+     * @param bool   $all_tabs Show on all tabs
+     *
+     * @return NoticeField
+     */
     public function __construct($name, $content, $class = 'notice', $all_tabs = true)
     {
         $classes = [
@@ -18,7 +30,7 @@ class NoticeField extends LiteralField
         if ($all_tabs) {
             array_push($classes, 'persist');
         }
-        $content = '<div class="' . implode($classes, ' ') . '">' . $content . '</div>';
+        $content = '<div class="' . implode(' ', $classes) . '">' . $content . '</div>';
 
         parent::__construct($name, $content);
     }
